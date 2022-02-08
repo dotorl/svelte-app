1e63d2607293469ec2f2ec5ada53f49fa8336edd
choks <choks0225@naver.com>
2022-02-08 22:58:45 +0900

    input 단방향, 양방향

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
 	export let name;
+	name = 'choks';
+
+	function assign() {
+		name = 'asdfasfd';
+	}
 </script>
 
 <main>
@@ -7,12 +12,22 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
+<button on:click={assign}>assign</button>
+
+<!-- 단방향 바인딩 -->
+<!-- <input type="text" value={name} on:input={e => (name = e.target.value)} /> -->
+
+<!-- 양방향 바인딩 -->
+<input type="text" bind:value={name} />
+
+
+
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+		margin: 0 auto
 	}
 
 	h1 {
